4fed58c56dbc1730dd2af49321a0234a0e38e31a
JavierOramas <javiale2000@gmail.com>
2022-05-02 08:05:03 -0400

    a lot of divisions

--- main.py
+++ main.py
@@ -80,16 +80,151 @@ def scrap_page(url):
                 k['Continent'] = str(
                     data)[s+len("Continent:</strong>"):e]
 
-                s = str(data).find("Region:</strong>")
+#
+                s = str(data).find("Atoll:</strong>")
                 e = str(data).find("</div>", s+1)
-                k['Region'] = str(
-                    data)[s+len("Region:</strong>"):e]
+                temp = str(data)[s+len("Atoll:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['Atoll'] = temp
 
                 s = str(data).find("Canton:</strong>")
                 e = str(data).find("</div>", s+1)
-                k['Canton'] = str(
-                    data)[s+len("Canton:</strong>"):e]
+                temp = str(data)[s+len("Canton:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['Canton'] = temp
 
+                s = str(data).find("Commune:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("Commune:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['Commune'] = temp
+
+                s = str(data).find("Corporation:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("Corporation:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['Corporation'] = temp
+
+                s = str(data).find("Council:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("Council:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['Council'] = temp
+
+                s = str(data).find("County:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("County:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['County'] = temp
+
+                s = str(data).find("Department:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("Department:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['Department'] = temp
+
+                s = str(data).find("District:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("District:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['District'] = temp
+
+                s = str(data).find("Division:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("Division:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['Division'] = temp
+
+                s = str(data).find("Emirate:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("Emirate:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['Emirate'] = temp
+
+                s = str(data).find("Governorate:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("Governorate:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['Governorate'] = temp
+
+                s = str(data).find("Island:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("Island:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['Island'] = temp
+
+                s = str(data).find("Municipality:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("Municipality:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['Municipality'] = temp
+
+                s = str(data).find("Parish:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("Parish:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['Parish'] = temp
+
+                s = str(data).find("Periphery:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("Periphery:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['Periphery'] = temp
+
+                s = str(data).find("Prefecture:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("Prefecture:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['Prefecture'] = temp
+
+                s = str(data).find("Province:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("Province:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['Province'] = temp
+
+                s = str(data).find("Quarter:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("Quarter:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['Quarter'] = temp
+
+                s = str(data).find("Rayon:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("Rayon:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['Rayon'] = temp
+
+                s = str(data).find("Region:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("Region:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['Region'] = temp
+
+                s = str(data).find("Section:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("Section:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['Section'] = temp
+
+                s = str(data).find("State:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("State:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['State'] = temp
+
+                s = str(data).find("State/Province:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("State/Province:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['State/Province'] = temp
+
+                s = str(data).find("Sub-Division:</strong>")
+                e = str(data).find("</div>", s+1)
+                temp = str(data)[s+len("Sub-Division:</strong>"):e]
+                if temp[:len("lass")+1] != 'lass':
+                    k['Sub-Division'] = temp
+#
                 s = str(data).find("Full Location:</strong>")
                 e = str(data).find("</div>", s+1)
                 k['Full Location'] = str(
